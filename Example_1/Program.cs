1610a4d35c5a85a89db1175f6bfe99c0e4c2af5c
Sergey <skokovs@inbox.ru>
2022-06-24 22:25:20 +0400

    HomeWork_seminar1

--- Example_1/Program.cs
+++ Example_1/Program.cs
@@ -1,27 +1,44 @@
-﻿Console.WriteLine("Введите номер задачи ('2', '4', '6' или'8'): ");
-
-int task_number = int.Parse(Console.ReadLine());
-
-switch (task_number)
+﻿string work_programm = "старт";
+while (work_programm.ToLower() != "q")
 {
-    case 2:
-        Task_2();
-        break;
-    case 4:
-        Task_4();
-        break;
-    case 6:
-        Task_6();
-        break;
-    case 8:
-        Task_8();
-        break;
-    default:
-        Console.WriteLine("Вы ввели неверное значение");
-        break;
+    Console.WriteLine(" ");
+    Console.WriteLine("Введите номер задачи ('2', '4', '6', '7', 8'): ");
+    Console.WriteLine(" ");
+    Console.WriteLine("или наберите 'q' для завершения");
+
+    work_programm = Console.ReadLine();
+
+    if (work_programm.ToLower() == "q") return;
+
+    int task_number = int.Parse(work_programm);
+
+    switch (task_number)
+    {
+        case 2:
+            Task_2();
+            break;
+        case 4:
+            Task_4();
+            break;
+        case 6:
+            Task_6();
+            break;
+        case 7:
+            Task_7();
+            break;
+        case 8:
+            Task_8();
+            break;
+
+        default:
+            Console.WriteLine("Вы ввели неверное значение");
+            break;
+    }
+
 }
 
-static void Task_2()
+
+static void Task_2()//Программа, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 {
     int max, min;
 
@@ -44,9 +61,9 @@ static void Task_2()
         min = secondValue;
     }
     Console.WriteLine("Максимальное число: " + max + " Минимальное число: " + min);
-}//Программа, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+}
 
-static void Task_4()
+static void Task_4()//Программа, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 {
     int max;
 
@@ -64,49 +81,42 @@ static void Task_4()
 
     if (firstValue < secondValue)
     {
-        if (secondValue < thirdValue)
-        {
-            max = thirdValue;
-        }
-        else
-        {
-            max = secondValue;
-        }
+        if (secondValue < thirdValue) max = thirdValue;
+
+        else max = secondValue;
     }
-    else
-    {
-        max = firstValue;
-    }
+    else max = firstValue;
+
     Console.WriteLine("Максимальное число: " + max);
-}//Программа, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+}
 
-static void Task_6()
+static void Task_6()//Программа, которая на вход принимает число и выдаёт, является ли число чётным.
 {
     Console.WriteLine("Введите число: ");
 
     int value = int.Parse(Console.ReadLine());
 
-    if (value % 2 == 0)
-    {
-        Console.WriteLine("Число " + value + " чётное");
-    }
-    else
-    {
-        Console.WriteLine("Число " + value + " нечётное");
-    }
-}//Программа, которая на вход принимает число и выдаёт, является ли число чётным.
+    if (value % 2 == 0) Console.WriteLine("Число " + value + " чётное");
 
-static void Task_8()
-{
-    Console.WriteLine("Введите число: ");
+    else Console.WriteLine("Число " + value + " нечётное");
+}
 
-    int value = int.Parse(Console.ReadLine());
-
-    for (int i = 1; i <= value; i++)
-    {
-        if (i % 2 == 0)
+static void Task_7(//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
         {
-            Console.WriteLine(i);
+            Console.WriteLine("Введите трехзначное число: ");
+
+int value = int.Parse(Console.ReadLine());
+
+int result = value / 100;
+
+Console.WriteLine("Последнее цифра данного числа: " + result);
         }
-    }
-}//Программа, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+        static void Task_8()//Программа, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+{
+    Console.WriteLine("Введите число: ");
+
+    int value = int.Parse(Console.ReadLine());
+
+    for (int i = 1; i <= value; i++) if (i % 2 == 0) Console.WriteLine(i);
+}
