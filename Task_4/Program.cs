aae191ef652b1dc8d6e25dd8fe92863c728a0ee5
SergeySkokov <skokovs@inbox.ru>
2022-07-01 01:33:21 +0400

    Task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,27 @@
+﻿int max;
+
+    Console.WriteLine("Программа, которая принимает на вход три числа и выдаёт максимальное из этих чисел.");
+
+    Console.WriteLine(" ");
+
+    Console.WriteLine("Введите первое число: ");
+
+    int firstValue = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите второе число: ");
+
+    int secondValue = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите третье число: ");
+
+    int thirdValue = int.Parse(Console.ReadLine());
+
+    if (firstValue < secondValue)
+    {
+        if (secondValue < thirdValue) max = thirdValue;
+
+        else max = secondValue;
+    }
+    else max = firstValue;
+
+    Console.WriteLine("Максимальное число: " + max);
