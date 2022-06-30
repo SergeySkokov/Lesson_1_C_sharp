6245b2a0341eb0d18c02967fc8d98c69cf89f3eb
SergeySkokov <skokovs@inbox.ru>
2022-07-01 01:34:32 +0400

    Task_7

---
+++ Task_7/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Программа, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.");
+
+    Console.WriteLine(" ");
+
+    Console.WriteLine("Введите трехзначное число: ");
+
+    int value = int.Parse(Console.ReadLine());
+
+    int result = value / 100;
+
+    Console.WriteLine("Последнее цифра данного числа: " + result);
