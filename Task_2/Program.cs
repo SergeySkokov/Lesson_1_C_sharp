1bb53ec2cb44f1282fb1ce40d9c92d533d812580
SergeySkokov <skokovs@inbox.ru>
2022-07-01 01:32:25 +0400

    Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,25 @@
+﻿int max, min;
+
+Console.WriteLine("Программа, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.");
+
+Console.WriteLine(" ");
+
+Console.WriteLine("Введите первое число: ");
+
+int firstValue = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+
+int secondValue = int.Parse(Console.ReadLine());
+
+if (firstValue < secondValue)
+{
+    max = secondValue;
+    min = firstValue;
+}
+else
+{
+    max = firstValue;
+    min = secondValue;
+}
+Console.WriteLine("Максимальное число: " + max + " Минимальное число: " + min);
