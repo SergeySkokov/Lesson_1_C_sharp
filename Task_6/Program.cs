52889c3a2095c104efb1cec09c374f82ab5ca47a
SergeySkokov <skokovs@inbox.ru>
2022-07-01 01:34:02 +0400

    Task_6

---
+++ Task_6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Программа, которая на вход принимает число и выдаёт, является ли число чётным.");
+
+    Console.WriteLine(" ");
+
+    Console.WriteLine("Введите число: ");
+
+    int value = int.Parse(Console.ReadLine());
+
+    if (value % 2 == 0) Console.WriteLine("Число " + value + " чётное");
+
+    else Console.WriteLine("Число " + value + " нечётное");
