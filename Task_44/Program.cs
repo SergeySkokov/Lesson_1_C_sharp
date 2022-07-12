3e65c6fc77e38e7494d04111b9eca89d8b3d8170
SergeySkokov <skokovs@inbox.ru>
2022-07-12 19:31:11 +0400

    Task 44

---
+++ Task_44/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число: ");
+int numberN = int.Parse(Console.ReadLine());
+int firstFib = 0;
+int secondFib = 1;
+int valueForSequence;
+Console.WriteLine($"Последовательность чисел Фибоначчи для числа {numberN} ");
+Console.Write($"{firstFib}, {secondFib}, ");
+for (int i = 1; i < numberN; i++)
+{
+    valueForSequence = secondFib + firstFib;
+    firstFib = secondFib; ;
+    secondFib = valueForSequence;
+    Console.Write(valueForSequence);
+    if (i + 1 != numberN) Console.Write(", ");
+    else break;
+}
+Console.WriteLine();
